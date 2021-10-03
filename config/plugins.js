ba52a4bff6a54f3f33e62f88a13a3892b75ff77a
Victor Oliveira <vamo89@gmail.com>
2021-10-03 15:20:13 -0300

    Change image upload location to cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_CLOUD_NAME"),
+      api_key: env("CLOUDINARY_API_KEY"),
+      api_secret: env("CLOUDINARY_API_SECRET"),
+    },
+    actionOptions: {
+      upload: { folder: "strapi-starter-mongo" },
+    },
+  },
+});
